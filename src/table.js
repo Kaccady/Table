44fd8bdaa68d0c33a2b23a02bb1186a1c2625ce5
Kaccady <z-rz@mail.com>
2019-04-24 21:29:55 +0300

    видимость столбцов, частично перетаскивание

--- src/table.js
+++ src/table.js
@@ -13,7 +13,10 @@ export default class Table extends Component {
       contextMenuisible: ["collapse", "collapse"],
       coordinates: [0, 0],
       target: {},
-      visibleHeads: new Array(6).fill(false)
+      visibleHeads: new Array(6).fill(false),
+      dragNDropOrder: [0, 1, 2, 3, 4, 5, 6],
+      dragMovingStart: 0,
+      dragMovingStop: 0
     };
   }
   componentDidMount() {
@@ -21,8 +24,43 @@ export default class Table extends Component {
     window.addEventListener("mouseup", this.handleContextDrop);
     window.addEventListener("click", this.handleClick);
     window.addEventListener("blur", this.hadleBlur);
+    window.addEventListener("dragstart", this.handleDragStart);
+    window.addEventListener("dragenter", this.handleDragDropMove);
+    //window.addEventListener("dragend", this.handleDragDrop);
+
     this.loadMore();
   }
+
+
+  handleDragStart = event => {
+    if (event.target.tagName === "TH") {
+      this.setState({ dragMovingStart: +event.target.getAttribute("number") });
+      console.log(this.state.dragMovingStart);
+    }
+  };
+
+  upduteTableOrders = () => {
+    let newOrder = this.state.dragNDropOrder.slice();
+    let start = this.state.dragNDropOrder[this.state.dragMovingStart];
+    let stop = this.state.dragNDropOrder[this.state.dragMovingStop];
+    [newOrder[start],newOrder[stop]]=[newOrder[stop],newOrder[start]];
+    this.setState({
+      dragNDropOrder: newOrder,
+    },()=>this.setState({dragMovingStart:stop})
+    );
+  };
+
+  handleDragDropMove = event => {
+    if (event.target.tagName === "TH") {
+      this.setState(
+        { dragMovingStop: +event.target.getAttribute("number") },
+        () => {
+          console.log(this.state.dragMovingStop);
+          this.upduteTableOrders();
+        }
+      );
+    }
+  };
 
   hadleBlur = event => {
     if (event.target.tagName === "TD") {
@@ -32,8 +70,10 @@ export default class Table extends Component {
   };
 
   handleContextDrop = event => {
-    if(event){
-    if(event.target.tagName === "LABEL"||event.target.tagName === 'INPUT')return;}
+    if (event) {
+      if (event.target.tagName === "LABEL" || event.target.tagName === "INPUT")
+        return;
+    }
     this.setState({
       contextMenuisible: ["collapse", "collapse"]
     });
@@ -165,51 +205,79 @@ export default class Table extends Component {
             <thead onContextMenu={this.handleContext}>
               <tr>
                 <th
+                  number="0"
+                  style={{ order: this.state.dragNDropOrder[0] }}
+                  draggable="true"
                   hidden={this.state.visibleHeads[0]}
                   onClick={this.handleSort}
                   id="name"
+                  className="name"
                 >
                   name
                 </th>
                 <th
+                  number="1"
+                  style={{ order: this.state.dragNDropOrder[1] }}
+                  draggable="true"
                   hidden={this.state.visibleHeads[1]}
                   onClick={this.handleSort}
                   id="birth_year"
+                  className="birth_year"
                 >
                   birth year
                 </th>
                 <th
+                  number="2"
+                  style={{ order: this.state.dragNDropOrder[2] }}
+                  draggable="true"
                   hidden={this.state.visibleHeads[2]}
                   onClick={this.handleSort}
                   id="eye_color"
+                  className="eye_color"
                 >
                   eye color
                 </th>
                 <th
+                  number="3"
+                  style={{ order: this.state.dragNDropOrder[3] }}
+                  draggable="true"
                   hidden={this.state.visibleHeads[3]}
                   onClick={this.handleSort}
                   id="gender"
+                  className="gender"
                 >
                   gender
                 </th>
                 <th
+                  number="4"
+                  style={{ order: this.state.dragNDropOrder[4] }}
+                  draggable="true"
                   hidden={this.state.visibleHeads[4]}
                   onClick={this.handleSort}
                   id="hair_color"
+                  className="hair_color"
                 >
                   hair color
                 </th>
                 <th
+                  number="5"
+                  style={{ order: this.state.dragNDropOrder[5] }}
+                  draggable="true"
                   hidden={this.state.visibleHeads[5]}
                   onClick={this.handleSort}
                   id="height"
+                  className="height"
                 >
                   height
                 </th>
                 <th
+                  number="6"
+                  style={{ order: this.state.dragNDropOrder[6] }}
+                  draggable="true"
                   hidden={this.state.visibleHeads[6]}
                   onClick={this.handleSort}
                   id="skin_color"
+                  className="skin_color"
                 >
                   skin color
                 </th>
@@ -218,13 +286,55 @@ export default class Table extends Component {
             <tbody onContextMenu={this.handleContext} onBlur={this.hadleBlur}>
               {this.state.heroes.sort(this.sorting).map((item, index) => (
                 <tr key={index}>
-                  <td hidden={this.state.visibleHeads[0]}>{item.name}</td>
-                  <td hidden={this.state.visibleHeads[1]}>{item.birth_year}</td>
-                  <td hidden={this.state.visibleHeads[2]}>{item.eye_color}</td>
-                  <td hidden={this.state.visibleHeads[3]}>{item.gender}</td>
-                  <td hidden={this.state.visibleHeads[4]}>{item.hair_color}</td>
-                  <td hidden={this.state.visibleHeads[5]}>{item.height}</td>
-                  <td hidden={this.state.visibleHeads[6]}>{item.skin_color}</td>
+                  <td
+                    style={{ order: this.state.dragNDropOrder[0] }}
+                    className="name"
+                    hidden={this.state.visibleHeads[0]}
+                  >
+                    {item.name}
+                  </td>
+                  <td
+                    style={{ order: this.state.dragNDropOrder[1] }}
+                    className="birth_year"
+                    hidden={this.state.visibleHeads[1]}
+                  >
+                    {item.birth_year}
+                  </td>
+                  <td
+                    style={{ order: this.state.dragNDropOrder[2] }}
+                    className="eye_color"
+                    hidden={this.state.visibleHeads[2]}
+                  >
+                    {item.eye_color}
+                  </td>
+                  <td
+                    style={{ order: this.state.dragNDropOrder[3] }}
+                    className="gender"
+                    hidden={this.state.visibleHeads[3]}
+                  >
+                    {item.gender}
+                  </td>
+                  <td
+                    style={{ order: this.state.dragNDropOrder[4] }}
+                    className="hair_color"
+                    hidden={this.state.visibleHeads[4]}
+                  >
+                    {item.hair_color}
+                  </td>
+                  <td
+                    style={{ order: this.state.dragNDropOrder[5] }}
+                    className="height"
+                    hidden={this.state.visibleHeads[5]}
+                  >
+                    {item.height}
+                  </td>
+                  <td
+                    style={{ order: this.state.dragNDropOrder[6] }}
+                    className="skin_color"
+                    hidden={this.state.visibleHeads[6]}
+                  >
+                    {item.skin_color}
+                  </td>
                 </tr>
               ))}
             </tbody>
